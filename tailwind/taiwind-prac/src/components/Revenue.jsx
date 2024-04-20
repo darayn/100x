102c2f62f74b1485b9e939abce1f5bb56d601248
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2024-04-20 12:15:09 +0530

    card refactor internal elements and grid

--- tailwind/taiwind-prac/src/components/Revenue.jsx
+++ tailwind/taiwind-prac/src/components/Revenue.jsx
@@ -5,18 +5,29 @@ export const RevenueCard = ({
     count
 }) => {
     return <div className="bg-white rounded shadow-md p-4">
-        <div className="text-grey-700">
-            {title}
-            ?
-        </div>
-        <div className="flex justify-between">
+        <div className="text-grey-700 flex justify-center flex-col">
+            <div className="flex">
+
             <div>
+                {title}
+            </div>
+            <div className="ml-1 flex juistify-center  flex-col">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                </svg>
+            </div>
+            </div>
+
+        </div>
+        <div className="flex justify-between pt-2">
+            <div className="font-semibold text-2xl">
                 ₹ {amount}
             </div>
             {orderCount ?
                 <div>
                     <div className="flex">
-                        <div className="text-blue-700 cursor-pointer underline">
+                        <div className="text-blue-700">
                             {orderCount} order(s)
                         </div>
                         <div>
