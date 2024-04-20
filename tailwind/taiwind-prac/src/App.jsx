5ff0f5cf691717582fe0206ace7aef41be78c503
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2024-04-20 19:20:36 +0530

    [WIP]: card complete

--- tailwind/taiwind-prac/src/App.jsx
+++ tailwind/taiwind-prac/src/App.jsx
@@ -4,14 +4,25 @@ import { RevenueCard } from './components/Revenue'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const nextPayout = {
+    nextPayoutDate : new Date(new Date().getTime() + (2 * 60 * 60 * 1000)),
+  }
   return (
     <>
       <div className='grid grid-cols-3'>
+      <RevenueCard
+          title={"Next Payout"}
+          amount={"92,312.20"}
+          orderCount={29}
+          nextPayout={nextPayout}></RevenueCard>
         <RevenueCard
           title={"Amount Pending"}
           amount={"92,312.20"}
           orderCount={29}></RevenueCard>
+        <RevenueCard
+          title={"Amount Pending"}
+          amount={"23,92,312.20"}
+          ></RevenueCard>
       </div>
     </>
   )
--- tailwind/taiwind-prac/src/components/Revenue.jsx
+++ tailwind/taiwind-prac/src/components/Revenue.jsx
@@ -1,43 +1,60 @@
+import moment from 'moment';
+
 export const RevenueCard = ({
     title,
     orderCount,
     amount,
-    count
+    nextPayout
 }) => {
-    return <div className="bg-white rounded shadow-md p-4">
-        <div className="text-grey-700 flex justify-center flex-col">
-            <div className="flex">
+    var humanizedTime = null;
+    if(nextPayout){
+        var humanizedTime = moment(nextPayout.nextPayoutDate).fromNow();
+    }
 
-            <div>
-                {title}
-            </div>
-            <div className="ml-1 flex juistify-center  flex-col">
+    return <div className="bg-white">
+        <div className="bg-transparent rounded shadow-md p-4">
+            <div className="text-grey-700 flex justify-center flex-col">
+                <div className="flex items-center">
+                    <div>
+                        {title}
+                    </div>
+                    <div className="ml-1 flex juistify-center  flex-col">
 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                </svg>
-            </div>
-            </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                        </svg>
+                    </div>
+                </div>
 
+            </div>
+            <div className="flex justify-between pt-2">
+                <div className="font-semibold text-2xl">
+                    ₹ {amount}
+                </div>
+                {orderCount ?
+                    <div>
+                        <div className="flex">
+                            <div className="text-blue-700 underline">
+                                {orderCount} order(s)
+                            </div>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-blue-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div> : null
+                }
+            </div>
         </div>
-        <div className="flex justify-between pt-2">
-            <div className="font-semibold text-2xl">
-                ₹ {amount}
-            </div>
-            {orderCount ?
-                <div>
-                    <div className="flex">
-                        <div className="text-blue-700">
-                            {orderCount} order(s)
-                        </div>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-blue-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                            </svg>
-                        </div>
+        {nextPayout ?
+                <div className="bg-red-100 rounded shadow-md px-6">
+                    <div className="flex justify-between">
+                        <p>Next Payout date:</p>
+                        <p>{humanizedTime}</p>
                     </div>
                 </div> : null
             }
-        </div>
     </div>
+    
 }
